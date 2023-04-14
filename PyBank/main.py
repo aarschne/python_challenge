74e8ffb85a5adf3765280521e681253cf7b4796e
aarschne <118419071+aarschne@users.noreply.github.com>
2023-04-14 00:50:19 -0500

    Added functionality to find the average differences and changed around some variable names

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,7 @@
 import csv
 import os
 
+#declare the path to the csv of bank values
 py_bank_csv_path = os.path.join(os.path.dirname(__file__), 'Resources', 'budget_data.csv')
 
 
@@ -14,18 +15,37 @@ with open(py_bank_csv_path, 'r') as csvfile:
     header = next(csvreader)
 
     #initialize variables
-    sum = 0
+    sum_profit_losses = 0
     num_months = 0
-    changes = []
-    current_change = 0
+    profit_losses = []
+    current_profit_loss = 0
 
-    # Loop through the data
+    # Loop through the csv data
     for row in csvreader:
-        current_change = int(row[1])
-        sum += current_change
+        current_profit_loss = int(row[1])
+        sum_profit_losses += current_profit_loss
         num_months += 1
-        changes.append(current_change)
+        profit_losses.append(current_profit_loss)
 
+    #initialize difference list
+    differences = []
+
+    #iterate over the profits and create a list of the differences
+    for i in range(len(profit_losses)-1):
+        differences.append(profit_losses[i+1]-profit_losses[i])
+    
+    #initialize the sum of the differences to find the average
+    sum_differences = 0
+
+    #find the sum
+    for diff in differences:
+        sum_differences += diff
+    
+    #find the average difference
+    average_diff = sum_differences/len(differences)
+    
+    #print statments to test the correctness of the values
+    print(round(average_diff,2))
     print(num_months)
-    print(sum)
-    print(changes)
+    print(sum_profit_losses)
+    
