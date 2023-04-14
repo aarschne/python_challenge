bb98c96b290fefc67331a80b2316ffbfa8b48587
aarschne <118419071+aarschne@users.noreply.github.com>
2023-04-14 09:41:59 -0500

    Finished up an initial draft of election

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,7 +9,7 @@ with open(election_data_path, 'r') as csvfile:
     #skip the header
     header = next(csvreader)
 
-    #initialize candidate list and num rows
+    #initialize candidate dict. (tot votes, percent) and num rows
     candidate_dict = {}
     num_votes = 0
 
@@ -28,9 +28,7 @@ with open(election_data_path, 'r') as csvfile:
         else:
             candidate_dict[current_candidate][0] += 1
         
-#find the percentages of the candidates' votes and store them in the dict
-for candidate in candidate_dict:
-    candidate_dict[candidate][1] = round(candidate_dict[candidate][0]*100/num_votes,3)
+
 
 #start finding the output string
 output_string = f'''\
@@ -40,10 +38,34 @@ Total Votes:  {num_votes}
 -------------------------\
 '''
 
+#initialize the winner
+winner = ""
+
 for candidate in candidate_dict:
+#find the percentages of the candidates' votes and store them in the dict    
+    candidate_dict[candidate][1] = round(candidate_dict[candidate][0]*100/num_votes,3)
+#add in each of the candidates with their names, total votes, and percentages
     output_string = "".join([output_string, 
     f"\n{candidate}: {candidate_dict[candidate][1]}% ({candidate_dict[candidate][0]})"])
+    #find winner: start by assuming it's first person, then update as you go along
+    if winner == "":
+        winner = candidate
+    elif candidate_dict[candidate][0] > candidate_dict[winner][0]:
+        winner = candidate
 
-output_string += "\n-------------------------"
+#add in final part of output string
+output_string += f'''\
+\n-------------------------
+Winner: {winner}
+-------------------------
+'''
 
+#print output string
 print(output_string)
+
+#make path to the output .txt file
+election_results_path = os.path.join(os.path.dirname(__file__), 'Election_Results.txt')
+
+#output string to a file
+with open(election_results_path,'w') as f:
+    f.write(output_string)
