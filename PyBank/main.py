faa01efd205d2e9dab85bb59ac7142d5792367e9
aarschne <118419071+aarschne@users.noreply.github.com>
2023-04-14 07:50:28 -0500

    Found the months associated with the greatest and lowest increases

--- PyBank/main.py
+++ PyBank/main.py
@@ -25,27 +25,31 @@ with open(py_bank_csv_path, 'r') as csvfile:
         current_profit_loss = int(row[1])
         sum_profit_losses += current_profit_loss
         num_months += 1
-        profit_losses.append(current_profit_loss)
+        profit_losses.append([row[0],current_profit_loss])
 
     #initialize difference list
     differences = []
 
     #iterate over the profits and create a list of the differences
     for i in range(len(profit_losses)-1):
-        differences.append(profit_losses[i+1]-profit_losses[i])
+        differences.append([profit_losses[i+1][0], profit_losses[i+1][1]-profit_losses[i][1]])
     
     #initialize the sum of the differences and the extreme values
     sum_differences = 0
-    max_diff = differences[0]
-    min_diff = differences[0]
+    max_diff = differences[0][1]
+    month_max_diff = differences[0][0]
+    min_diff = differences[0][1]
+    month_min_diff = differences[0][0]
 
     #find the sum and the extreme values
     for diff in differences:
-        sum_differences += diff
-        if diff < min_diff:
-            min_diff = diff
-        if diff > max_diff:
-            max_diff = diff
+        sum_differences += diff[1]
+        if diff[1] < min_diff:
+            min_diff = diff[1]
+            month_min_diff = diff[0]
+        if diff[1] > max_diff:
+            max_diff = diff[1]
+            month_max_diff = diff[0]
     
     #find the average difference
     average_diff = sum_differences/len(differences)
@@ -56,6 +60,8 @@ with open(py_bank_csv_path, 'r') as csvfile:
     print(round(average_diff,2))
     print(num_months)
     print(sum_profit_losses)
+    print(month_max_diff)
     print(max_diff)
+    print(month_min_diff)
     print(min_diff)
     
